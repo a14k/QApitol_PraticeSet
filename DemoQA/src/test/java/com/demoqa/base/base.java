60c35155998afda786122b5f18065ca58bdb73ba
a14k <ankitaambast30@gmail.com>
2024-11-11 23:33:12 +0530

    Demo QA ele and forms

--- DemoQA/src/test/java/com/demoqa/base/base.java
+++ DemoQA/src/test/java/com/demoqa/base/base.java
@@ -1,34 +1,56 @@
 package com.demoqa.base;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.Pages;
+
+import java.io.*;
+import java.util.Properties;
 public class base {
+    public static WebDriver driver;
+    public static Properties properties;
 
-
-    public WebDriver driver;
-    @BeforeClass
-    public void beforeTest() throws IOException {
+    @BeforeMethod
+    public static void initialize() throws IOException {
         driver = new ChromeDriver();
-        System.out.println("Before Test");
-        driver.get("https://demoqa.com");
+        //driver.get("https://demoqa.com/elements");
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
-    }
-    @AfterClass
-    public void afterTest(){
-        System.out.println("After Test");
-        // driver.close();
     }
 
+    @AfterMethod
+    public static void quit() {
+        // driver.quit();
+    }
+
+    public static void  wait1(String Xpath) {
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Xpath)));
+    }
+
+    public static void scroll(int val,int val1){
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy("+val+","+val1+")");
+
+
+    }
 }
+
+
+
+
+
--- DemoQA/src/test/java/page/Pages.java
+++ DemoQA/src/test/java/page/Pages.java
@@ -1,224 +1,136 @@
 package page;
 
 import com.demoqa.base.base;
-import dev.failsafe.internal.util.Durations;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.net.URL;
 import java.util.List;
-import java.util.Set;
 
 public class Pages extends base {
-
     @Test
-    public void textBoxTest() {
-        System.out.println("HomePage Test");
-        driver.findElement(By.xpath("//div[text()='Elements']"));
-
-        /* SELECT TEXT_BOX  */
-
+    public void textbox() throws InterruptedException {
+        base.wait1("//input[@class=' mr-sm-2 form-control']");
         driver.findElement(By.xpath("//span[text()='Text Box']")).click();
-        driver.findElement(By.id("userName")).sendKeys("neha@gmail.com");
-        driver.findElement(By.id("userEmail")).sendKeys("neha123@gmail.com");
-        driver.findElement(By.id("currentAddress")).sendKeys("BLR");
-
-        driver.findElement(By.id("permanentAddress")).sendKeys("BBSR");
-        WebElement sumbitButton= driver.findElement(By.xpath("//button[text()='Submit']"));
-        // sumbitButton.click();
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", sumbitButton);
-
+        driver.findElement(By.xpath("//input[@class=' mr-sm-2 form-control']")).sendKeys("Ankita");
+        driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//textarea[@id='currentAddress']")).sendKeys("Address");
+        driver.findElement(By.xpath("//textarea[@id='permanentAddress']")).sendKeys("permananent Address");
+        //driver.findElement(By.xpath("//button[@id='submit']")).click();
+        base.scroll(0, 200);
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
     }
 
-    /*CheckBOX*/
-
     @Test
-    public void checkBoxTest() {
-        System.out.println("HomePage Test");
-        driver.findElement(By.xpath("//span[text()='Check Box']")).click();
-        WebElement checkBox= driver.findElement(By.xpath("//input[@id='tree-node-home']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", checkBox);
+    public void check() throws InterruptedException {
 
-
-        WebElement plusIcon =driver.findElement(By.xpath("//button[@aria-label='Expand all']//*[name()='svg']"));
-        plusIcon.click();
-
-    }
-    /*Radio Button*/
-
-    @Test
-    public void radioButtonTest() {
-        System.out.println("HomePage Test");
-        driver.findElement(By.xpath("//span[text()='Radio Button']")).click();
-        WebElement readioButton= driver.findElement(By.xpath("//input[@id='impressiveRadio']"));
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", readioButton);
-        // readioButton  .click();
+        base.wait1("//li[@class='btn btn-light active']");
+        driver.findElement(By.id("item-1")).click();
+        driver.findElement(By.xpath("//span[@class='rct-checkbox']")).click();
+        boolean selected = driver.findElement(By.xpath("//span[@class='rct-checkbox']")).isEnabled();
+        Assert.assertTrue(selected);
     }
 
-    /*WebTable*/
     @Test
-    public void webTablePageTest() {
-        System.out.println("HomePage Test");
+    public void raidobutton() {
+        driver.findElement(By.id("item-2")).click();
 
-        WebElement webTable = driver.findElement(By.xpath("//span[text()='Web Tables']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", webTable);
-
-        WebElement AddButton = driver.findElement(By.id("addNewRecordButton"));
-        JavascriptExecutor js1 = (JavascriptExecutor) driver;
-        js1.executeScript("arguments[0].click()", AddButton);
-
-        /* Register form */
-        driver.findElement(By.id("firstName")).sendKeys("Neha");
-        driver.findElement(By.id("lastName")).sendKeys("G");
-        driver.findElement(By.id("userEmail")).sendKeys("neha@gmail.com");
-        driver.findElement(By.id("age")).sendKeys("30");
-        driver.findElement(By.id("salary")).sendKeys("300000");
-        driver.findElement(By.id("department")).sendKeys("RMG");
-        WebElement sumbitButton= driver.findElement(By.xpath("//button[text()='Submit']"));
-        JavascriptExecutor js2 = (JavascriptExecutor) driver;
-        js2.executeScript("arguments[0].click()", sumbitButton);
-
-        /* Search Add Item*/
-        driver.findElement(By.id("searchBox")).sendKeys("Neha");
-        driver.findElement(By.id("basic-addon2")).click();
-
-        /*verify neha is present or not */
-        WebElement getEmail=driver.findElement(By.xpath("//div[text()='neha@gmail.com']"));
-
-        Assert.assertEquals(getEmail.getText(), "neha@gmail.com" ,"matched");
-        System.out.println("NEHA IS ADDED");
-
+        base.scroll(0, 200);
+        wait1("//label[@for='yesRadio']");
+        driver.findElement(By.xpath("//label[@for='yesRadio']")).click();
     }
-    /*Button Action related operation */
 
     @Test
-    public void buttonclickTest() throws InterruptedException {
+    public void button() {
+        base.scroll(0, 200);
+        driver.findElement(By.id("item-4")).click();
 
-        WebElement buttonText=driver.findElement(By.xpath("//span[text()='Buttons']"));
-        JavascriptExecutor js=(JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", buttonText);
-
-        WebElement doubleClickText = driver.findElement(By.xpath("//button[text()='Double Click Me']"));
-        WebElement rightClickText = driver.findElement(By.xpath("//button[text()='Right Click Me']"));
-
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Double Click Me']")));
-        //  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Right Click Me']")));
-
-        JavascriptExecutor js1=(JavascriptExecutor) driver;
-        js1.executeScript("arguments[0].scrollIntoView(true);", doubleClickText);
+        base.scroll(0, 200);
+        base.wait1("//div[@class='header-wrapper']");
 
 
-        Actions actions=new Actions(driver);
-        actions.doubleClick(doubleClickText).perform();
-        actions.contextClick(rightClickText).perform();
-
-        WebElement clickoption=driver.findElement(By.xpath("//button[text()='Click Me']"));
-        actions.click(clickoption).perform();
-
+        Actions act = new Actions(driver);
+        WebElement doubleclick = driver.findElement(By.xpath("//button[@id='doubleClickBtn']"));
+        act.doubleClick(doubleclick).build().perform();
+        WebElement rightclick = driver.findElement(By.id("rightClickBtn"));
+        act.contextClick(rightclick).build().perform();
+        WebElement click = driver.findElement(By.xpath("//button[@class='btn btn-primary'][.='Click Me']"));
+        act.click(click).build().perform();
     }
-    /* lINK RELATED OPERATION */
+
     @Test
-    public void LinksTest() {
-        System.out.println("HomePage Test");
-        WebElement link = driver.findElement(By.xpath("//span[text()='Links']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", link);
-        link.click();
+    public void links() {
+        base.scroll(0, 200);
+        driver.findElement(By.id("item-5")).click();
+        base.scroll(0, 200);
 
-        String originalWindow = driver.getWindowHandle();
-        System.out.println(originalWindow);
-
-        driver.findElement(By.xpath("//a[text()='Home' and @id='simpleLink']")).click();
-
-        Set<String> allId = driver.getWindowHandles();
-        System.out.println(allId);
-
-        for( String id : allId){
-            driver.switchTo().window(originalWindow);
-
+        List<WebElement> link = driver.findElements(By.xpath("//p/a"));
+        for (int i = 0; i < link.size(); i++) {
+            link.get(i).sendKeys(Keys.chord(Keys.CONTROL, Keys.ENTER));
         }
 
-        //driver.findElement(By.xpath("//a[text()='ZYX6y' and @id='dynamicLink' ]")).click();
-        WebDriverWait wait =new WebDriverWait(driver , Duration.ofSeconds(200));
-        WebElement dynamicLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='ZYX6y' and @id='dynamicLink']")));
-        dynamicLink.click();
+    }
 
-        Set<String> allIdd = driver.getWindowHandles();
-        System.out.println(allId);
+    @Test
+    public void brokenLinks() throws IOException, InterruptedException {
+        base.scroll(0, 500);
+        driver.findElement(By.id("item-6")).click();
 
-        for( String id : allIdd){
-            driver.switchTo().window(originalWindow);
+        List<WebElement> Links = driver.findElements(By.xpath("//p/following-sibling::a"));
+
+        for (WebElement link : Links) {
+
+            String url = link.getAttribute("href");
+
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            conn.setRequestMethod("HEAD");
+
+            conn.connect();
+
+            int repcode = conn.getResponseCode();
+
+            System.out.println(conn.getResponseCode());
+
 
         }
-
-
     }
     @Test
-    public void brokenLinksTest() throws InterruptedException {
-        System.out.println("HomePage Test");
-        // WebElement BrokenLink =  driver.findElement(By.xpath("//span[text() ='Broken Links - Images']"));
-        // BrokenLink .click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        WebElement BrokenLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Broken Links - Images']")));
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", BrokenLink);
-
-        JavascriptExecutor js2=(JavascriptExecutor) driver;
-        WebElement validLink = driver.findElement(By.xpath("//a[text()='Click Here for Valid Link']"));
-        js2.executeScript("arguments[0].click()", validLink);
-
-
-
-
+    public void Pratice_form()
+    {
+        base.scroll(0, 500);
+        driver.findElement(By.xpath("(//div[@class='header-wrapper'])[2]")).click();
+        driver.findElement(By.xpath("//span[text()='Practice Form']")).click();
+        driver.findElement(By.id("firstName")).sendKeys("Ankita");
+        driver.findElement(By.id("lastName")).sendKeys("Laki");
+        driver.findElement(By.id("userEmail")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//label[@for='gender-radio-2']")).click();
+        driver.findElement(By.id("userNumber")).sendKeys("1234567890");
+        WebElement dateField = driver.findElement(By.id("dateOfBirthInput"));
+        dateField.sendKeys("1999-15-04");
+        driver.findElement(By.id("subjectsContainer")).sendKeys("Selenium, java , TestNG");
+        driver.findElement(By.xpath("//label[@for='hobbies-checkbox-2']")).click();
+        driver.findElement(By.id("currentAddress")).sendKeys("123 laxmi appt");
+        WebElement image_Upload = driver.findElement(By.id("imageUpload"));
+        image_Upload.sendKeys("\"C:\\Users\\Qapitol\\Downloads\\shared image.jpg\"");
+        WebElement dropDown = driver.findElement(By.id("state"));
+        Select stateSelect = new Select(dropDown);
+        stateSelect.selectByVisibleText("NCR");
+        WebElement dropDown1 = driver.findElement(By.id("city"));
+        Select citySelect = new Select(dropDown1);
+        citySelect.selectByVisibleText("Delhi");
+        WebElement submit = driver.findElement(By.id("submit"));
+        submit.click();
     }
-    /* File upload And Download*/
-    @Test
-    public void checkUploadAndDownloadTest() throws InterruptedException {
-        System.out.println("HomePage Test");
-        WebElement uploadDownloadText=  driver.findElement(By.xpath("//span[text()='Upload and Download']"));
-        JavascriptExecutor js=(JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", uploadDownloadText);
-
-        WebElement download =driver.findElement(By.xpath("//a[text()='Download']"));
-        JavascriptExecutor js1=(JavascriptExecutor) driver;
-        js1.executeScript("arguments[0].click()", download);
-
-        WebElement uploadFILE=driver.findElement(By.xpath("//input[@id='uploadFile']"));
-        uploadFILE.sendKeys("C:\\Users\\Qapitol QA\\Downloads");
-
-    }
-
-    /* Find Dynamic Properties */
-    @Test
-    public void checkDynamicTest(){
-        System.out.println("HomePage Test");
-        WebElement dynElement= driver.findElement(By.xpath("//*[name()='svg']/following-sibling::span[text()='Dynamic Properties']"));
-        JavascriptExecutor js=(JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()", dynElement);
-
-    }
-
-
-
-
-
-
 
 
 
 }
+
