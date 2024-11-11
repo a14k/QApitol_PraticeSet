2ca9d89daf8c387f7d5b79a84df5646b4215ff7a
a14k <ankitaambast30@gmail.com>
2024-11-11 16:52:32 +0530

    New coded added

--- src/test/java/com/qapital/testcases/LoginPageTestCase.java
+++ src/test/java/com/qapital/testcases/LoginPageTestCase.java
@@ -1,5 +1,9 @@
 package com.qapital.testcases;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.qapital.base.BaseClass;
 import com.qapital.pages.LoginPage;
 import com.qapital.util.ReadExcel;
@@ -17,9 +21,16 @@ public class LoginPageTestCase {
     private static final Logger logger = (Logger) LogManager.getLogger(LoginPage.class);
         //LogManager.getLogger(LoginPageTestCase.class);
 
+    ExtentReports extent = new ExtentReports();
+    ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+
     LoginPage loginPage;
     @BeforeTest
     public void setUp() throws IOException {
+        Theme Theme = null;
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("Keyword driven Report");
+
         BaseClass.initialize();
         logger.debug("Entered into SetUp");
         loginPage = new LoginPage();
@@ -35,16 +46,24 @@ public class LoginPageTestCase {
     @Test(dataProvider = "data")
     public void testLogin(String user,
                           String pass) throws IOException, InterruptedException {
+
+        ExtentTest text = extent.createTest("Execution of Keyword Drivern Testing")
+                .assignAuthor("Mayuresh").assignCategory("Automation Test").assignDevice("Window");
+
+        text.info("Initization of Script");
         loginPage.login(user,pass);
         String expected = "QAPit0l";
-
+        text.pass("Validation completed");
         String actual = "QAPit0l";
         Assert.assertEquals(actual,expected,"This is not same");
     }
     @AfterTest
     public void tearDown() throws InterruptedException {
+
         Thread.sleep(5000);
         BaseClass.quit();
+
+        extent.flush();
     }
 }
 
